1a3c29385262834735d5497e2c118293235acd9d
Daiga G <80636937+daigag@users.noreply.github.com>
2022-11-24 11:31:05 +0200

    Update hide_and_seek.py

--- hide_and_seek.py
+++ hide_and_seek.py
@@ -1,10 +1,10 @@
-import os
-import pygame
+import pygame, os, random
 from pygame.locals import *
 from sound import *
-import random
 
 screen = pygame.display.set_mode((1000, 600))
+
+green = (0, 50, 0)
 
 class Pointer(pygame.sprite.Sprite):
     def __init__(self):
@@ -22,7 +22,6 @@ class Pointer(pygame.sprite.Sprite):
     def update(self):
         self.rect.center = pygame.mouse.get_pos()
 
-
 class Object(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__()
@@ -32,7 +31,6 @@ class Object(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image, (50, 50))
         self.rect = self.image.get_rect()
         self.rect.center = [pos_x, pos_y]
-
 
 class Hide_and_seek:
     
@@ -53,9 +51,11 @@ class Hide_and_seek:
     def __init__(self):
         pygame.init()
         self.running = True
-        
+     
+    def run(self):
+
         pygame.display.set_caption('Hide and seek')
-        icon = pygame.image.load('citi_atteli/an acorn.png')
+        icon = pygame.image.load('citi_atteli/a fly agaric.png')
         icon = pygame.display.set_icon(icon)
 
         background = screen.blit(Hide_and_seek.winter_img, (0, 0))
@@ -78,7 +78,6 @@ class Hide_and_seek:
 
         pygame.mouse.set_visible(False)
 
-    def run(self):
 
         while True:
             pygame.display.update()
@@ -87,22 +86,25 @@ class Hide_and_seek:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if event.button == K_ESCAPE:
-                        
-                        screen.fill((1, 68, 33))
+                elif event.type == pygame.KEYDOWN:
+                    if event.type == K_ESCAPE:
+                        pygame.display.quit()
+                        self.running = False
+                        screen.fill(green)
+                        pygame.display.set_caption('Forest Game by Zane and Daiga')
+                        icon = pygame.image.load('citi_atteli/a pine needle.png')
+                        icon = pygame.display.set_icon(icon)
                         return
-                    
-                    
-
-                if event.type == pygame.MOUSEBUTTONDOWN:
+            
+                elif event.type == MOUSEBUTTONDOWN:
                     Pointer().klick()
 
-            screen.blit(Hide_and_seek.winter_img, (0, 0))
-            Hide_and_seek.object_group.draw(screen)
-            Hide_and_seek.pointer_group.draw(screen)
-            Hide_and_seek.pointer_group.update()
+                screen.blit(Hide_and_seek.winter_img, (0, 0))
+                Hide_and_seek.object_group.draw(screen)
+                Hide_and_seek.pointer_group.draw(screen)
+                Hide_and_seek.pointer_group.update()
             pygame.display.flip()
+            pygame.display.update()
 
 
 if __name__ == '__main__':
